d6717f2bfd0c41860c03a3df5ff04daa897c3f0f
adkofgabriel <124207493+adkofgabriel@users.noreply.github.com>
2023-02-01 18:23:23 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log(document);
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto")
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
